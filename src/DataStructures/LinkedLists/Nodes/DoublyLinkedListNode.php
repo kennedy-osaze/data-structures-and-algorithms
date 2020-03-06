3781f3df80545a027df309d595b48c38b688aaf1
Kennedy Osaze <me.osaze@gmail.com>
2020-03-06 22:20:57 +0100

    Add linked list nodes

---
+++ src/DataStructures/LinkedLists/Nodes/DoublyLinkedListNode.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace KennedyOsaze\DataStructures\LinkedLists\Nodes;
+
+class DoublyLinkedListNode extends Node
+{
+    /**
+     * Holds reference to the previous node
+     *
+     * @var static|null
+     */
+    private $previous = null;
+
+    /**
+     * Sets the previous node reference
+     *
+     * @param static|null $node
+     *
+     * @return void
+     */
+    public function setPrevious(DoublyLinkedListNode $node = null)
+    {
+        $this->previous = $node;
+    }
+
+    /**
+     * Gets the previous node reference
+     *
+     * @return static|null
+     */
+    public function getPrevious()
+    {
+        return $this->previous;
+    }
+}
